2a71645d959d84b8f52cec8e21e4b2737b1950a5
jnuo35 <jnuo_chen@163.com>
2026-02-28 13:57:50 +0800

    Update header file jnuo.h with new comments

--- jnuo.h
+++ jnuo.h
@@ -1,25 +1,25 @@
 /*
-  2026  from jnuo      v1.0.0
-    - 持续制作中......
-    - Continuously in production .....
-	- 使用前请加上using namespace jnuo;
-	- Please add 'using namespace jnuo' before use;
-    - 功能1：日志功能
-    - Function 1: Log function
-      - 请先init_log_file("./你自定义的日志名.txt");
-      - Please first initw_file("./your custom log name. txt");
-      - 使用JNUO_LOG_日志类型("日志内容");
-      - Use JNUO_LOG_Log type("log content");
-        - 支持的日志类型有：DEBUG,INFO,ERROR,WARN
-        - The supported log types are DEBUG, INFO, ERROR, and WARN
-		- 也就是JNUO_LOG_DEBUG("日志内容");JNUO_LOG_INFO("日志内容");
-                JNUO_LOG_ERROR("日志内容");JNUO_LOG_WARN("日志内容");
-        - That is, JNUO_LOG_DEBUG("log content"); JNUO_LOG_INFO("log content");
-                   JNUO_LOG_ERROR("Log Content"); JNUO_LOG_WARN("Log Content");
-	  - 不需要日志功能是请close_log_file()
-	  - If the logging function is not required, please closew_log_file()
-    - GitHub的项目：https://github.com/jnuo35/header_file_jnuo
-    - Projects on GitHub: https://github.com/jnuo35/header_file_jnuo
+* 2026  from jnuo      v1.0.0
+*   - 持续制作中......
+*   - Continuously in production .....
+*   - 使用前请加上using namespace jnuo;
+*   - Please add 'using namespace jnuo' before use;
+*   - 功能1：日志功能
+*   - Function 1: Log function
+*     - 请先init_log_file("./你自定义的日志名.txt");
+*     - Please first initw_file("./your custom log name. txt");
+*     - 使用JNUO_LOG_日志类型("日志内容");
+*     - Use JNUO_LOG_Log type("log content");
+*       - 支持的日志类型有：DEBUG,INFO,ERROR,WARN
+*       - The supported log types are DEBUG, INFO, ERROR, and WARN
+*   	- 也就是JNUO_LOG_DEBUG("日志内容");JNUO_LOG_INFO("日志内容");
+*               JNUO_LOG_ERROR("日志内容");JNUO_LOG_WARN("日志内容");
+*       - That is, JNUO_LOG_DEBUG("log content"); JNUO_LOG_INFO("log content");
+*                  JNUO_LOG_ERROR("Log Content"); JNUO_LOG_WARN("Log Content");
+*     - 不需要日志功能是请close_log_file()
+*     - If the logging function is not required, please closew_log_file()
+*   - GitHub的项目：https://github.com/jnuo35/header_file_jnuo
+*   - Projects on GitHub: https://github.com/jnuo35/header_file_jnuo
 */
 #ifndef JNUO_H
 #define JNUO_H
@@ -128,4 +128,5 @@ do { if (jnuo::g_log_threshold <= jnuo::LogLevel::Error) \
 jnuo::log(jnuo::LogLevel::Error, __VA_ARGS__); } while(0)
 
 #endif
+
 
